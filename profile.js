febacaa383201e6a2cb5511df38ce58dc81c3ac5
Sudha Krishnamoorthy <sudhavkr@hotmail.com>
2022-09-20 21:18:24 -0500

    first commit profile.js

---
+++ profile.js
@@ -0,0 +1,17 @@
+const favColor = document.querySelector('#color')
+//console.log(favColor)
+
+favColor.addEventListener('click', function () {
+    alert('My favorite color is red')
+})
+
+const favPlace = document.querySelector('#place')
+favPlace.addEventListener('click', function() {
+    alert('My favorite place is Ladakh')
+})
+
+const favRitual = document.querySelector('#ritual')
+favRitual.addEventListener('click', function() {
+    alert('My favorite ritual is to meditate' )
+})
+
